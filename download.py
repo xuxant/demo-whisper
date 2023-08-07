a234601d36b10c5116acfca27d29a56f73106038
Susanta Gautam <xuxant02@gmail.com>
2023-08-07 10:48:49 +0545

    Check

--- download.py
+++ download.py
@@ -7,9 +7,10 @@ import torch
 
 def download_model():
     device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(device)
     model_size = "large-v2"
     compute_type = "float16"
-    model = WhisperModel(model_size, device, compute_type)
+    model = WhisperModel(model_size, device=device, compute_type=compute_type)
 
 
 if __name__ == "__main__":
